dde86beb786043f8b90f58a3853b48c49d87ee49
margaritasing <libiamfreitesm@gmail.com>
2022-07-20 12:12:40 -0300

    elimino archivo innecesarios

---
+++ src/componentes/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+    <h2>Formulario de login</h2>
+        <form>
+
+        
+        
+        </form>    
+    </div>
+  )
+}
+
+export default Login
