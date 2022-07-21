6bfd35bad7522395a4013789adc73f2126a99710
margaritasing <libiamfreitesm@gmail.com>
2022-07-21 11:26:04 -0300

    modifico el app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import { Routes, Route } from "react-router-dom";
 import Login from './componentes/Login';
+import Listado from "./componentes/Listado";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<Login />} />     
-    </Routes>    
+      <Route exact path="/" element={<Login />} />     
+      <Route path="/listado"  element={<Listado />} />
+    </Routes> 
+    </BrowserRouter>   
     </div>
   );
 }
