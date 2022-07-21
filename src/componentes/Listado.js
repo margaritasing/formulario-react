f5b45639e29f9a6b038f0f16c2eb797ee70701e6
margaritasing <libiamfreitesm@gmail.com>
2022-07-21 13:27:32 -0300

    agrego cambios al listado

--- src/componentes/Listado.js
+++ src/componentes/Listado.js
@@ -1,25 +1,35 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
+import { Link, Navigate } from "react-router-dom";
 
-const Listado = () => {
 
-  let navigate = useNavigate();
-
-  
+const Listado = () => {   
+  let token  = localStorage.getItem('token');
 
   useEffect(() => {
-    const token  = localStorage.getItem('token');
-    console.log(token)
-    if (token === null) {
-      navigate("/");    
-    }
-  });
+   const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=75b9f04bb9ba776a3e2318bbe7838f21&language=es-ES&page=1';
+  
+   
+  })
+  
   
   return (
-    <div>
-    <h2>Listado</h2>
+    <>
+    {!token && <Navigate replace to="/" />}
+    <div className="row my-2">
+        <div className="col-3">
+            <div className="card" style={{width: "18rem"}}>
+              <img src="..." className="card-img-top" alt="..."/>
+                <div className="card-body">
+                  <h5 className="card-title">Movie title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <Link to="" className="btn btn-dark">View details</Link>
+                </div>
+            </div>     
+        </div>     
     </div>
+    </>
+    
   )
 }
 
