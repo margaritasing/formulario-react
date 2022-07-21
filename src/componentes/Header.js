33edf86e53f45883b75bd257a2a5f70cdc7e11af
margaritasing <libiamfreitesm@gmail.com>
2022-07-21 13:25:27 -0300

    agrego el componente header

---
+++ src/componentes/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+
+
+       <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
+        <div className="container-fluid">            
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
+                    <Link to="/listado" className="nav-link" >Listado</Link>
+                    <Link to="/contacto"  className="nav-link">Contacto</Link>                
+                </div>
+            </div>
+        </div>
+        </nav>
+    
+       
+   
+
+  )
+}
+
+export default Header
