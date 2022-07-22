4705d7e8033ed11aa469be3d2b87b2ae3b53d21c
margaritasing <libiamfreitesm@gmail.com>
2022-07-22 13:38:58 -0300

    ...

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import {Link } from 'react-router-dom';
+import Buscador from './Buscador';
 
 const Header = () => {
   return (
 
 
        <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
-        <div className="container-fluid">            
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
-                    <Link to="/listado" className="nav-link" >Listado</Link>                                   
+            <div className="container-fluid">            
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
+                        <Link to="/listado" className="nav-link" >Listado</Link>                                   
+                    </div>
                 </div>
-            </div>
-        </div>
+               <Buscador />
+             </div>
         </nav>
     
        
