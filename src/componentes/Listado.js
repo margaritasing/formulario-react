69d913e3a5e33c3fd8eab94266a55944b87bddac
margaritasing <libiamfreitesm@gmail.com>
2022-07-22 16:07:19 -0300

    agrego el boton de favoritos

--- src/componentes/Listado.js
+++ src/componentes/Listado.js
@@ -3,9 +3,12 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, Navigate } from "react-router-dom";
 import swal from 'sweetalert';
+import styled from "styled-components";
 
 
-const Listado = () => {   
+
+const Listado = (props) => {   
+  console.log(props)
 
   let token  = sessionStorage.getItem('token');
 
@@ -27,30 +30,56 @@ const Listado = () => {
   
   
   return (
-    <>
+    
+    <Section>
     {!token && <Navigate replace to="/" />}
     <div className="row my-2 mx-2">
     {
         movieList.map( (oneMovie, index) => {
           return(
-               <div className="col-3" key={index}>
-                <div className="card my-2">
-                  <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                      <h5 className="card-title">{oneMovie.title.substring(0, 30)}</h5>
-                      <p className="card-text">{oneMovie.overview.substring(0, 100)}...</p>
-                      <Link to={`/detalle?movieID=${oneMovie.id}`} className="btn btn-dark">View details</Link>
-                    </div>
-                </div>     
-            </div>  
+            
+                  <div className="col-3" key={index}>
+                    <div className="card my-2">
+                      <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="..."/>
+                        <button className="favorito"
+                        onClick={props.addOrRemoveFromFavs}
+                        data-movie-id={oneMovie.id}>🖤
+                        </button>
+                      <div className="card-body">
+                          <h5 className="card-title">{oneMovie.title.substring(0, 30)}</h5>
+                          <p className="card-text">{oneMovie.overview.substring(0, 100)}...</p>
+                          <Link to={`/detalle?movieID=${oneMovie.id}`} className="btn btn-dark">View details</Link>
+                        </div>
+                    </div>     
+                  </div>           
+              
           )
         })
     }
          
     </div>
-    </>
+    </Section>
+    
     
   )
 }
+
+const Section = styled.section`   
+.favorito {
+  width: 30px;
+  height: 30px;
+  background-color: #f2f2f2;
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  border: none;
+  font-size:13px;
+}
+ 
+`;
 
 export default Listado
