b697dbde9c4bd9861bd1121fd86bede549766043
margaritasing <libiamfreitesm@gmail.com>
2022-07-25 10:48:30 -0300

    ....

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 
 import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
 import Login from './componentes/Login';
 import Listado from "./componentes/Listado";
 import Header from "./componentes/Header";
-import Footer from "./componentes/Footer";
 import Detalle from "./componentes/Detalle";
 import Resultados from "./componentes/Resultados";
+import Favoritos from "./componentes/Favoritos";
+import swal from 'sweetalert';
+import "./App.css"
 
 
-function App() {
+function App() { 
 
-  const favMovie = localStorage.getItem('favs');
+  const [favoritos, setFavoritos] = useState([]);
 
-  let tempMoviesInFavs;
-
-  if (favMovie === null) {
-    tempMoviesInFavs = []    
-  }else{    
-    tempMoviesInFavs = JSON.parse(favMovie)
-  }
-
-  console.log(tempMoviesInFavs)
+    useEffect(() => {
+        const favInLocal = localStorage.getItem('favs');
+       
+        if (favInLocal !== null) {
+          const favsArrays = JSON.parse(favInLocal);
+         
+          setFavoritos(favsArrays);        
+        }
+    }, [])
+    
 
   const addOrRemoveFromFavs=  e =>{
+
+    const favMovie = localStorage.getItem('favs');
+
+    let tempMoviesInFavs;
+  
+    if (favMovie === null) {
+      tempMoviesInFavs = []    
+    }else{    
+      tempMoviesInFavs = JSON.parse(favMovie)
+    }
+
+
     const btn = e.currentTarget;
     const parent = btn.parentElement;
     const imgURL = parent.querySelector('img').getAttribute('src');
@@ -33,36 +49,39 @@ function App() {
       id:btn.dataset.movieId
     }
 
-   /*  let movieIsInArray = tempMoviesInFavs.find(oneMovie =>{
+    let movieIsInArray = tempMoviesInFavs.find(oneMovie =>{
       return oneMovie.id === movieData.id
-    }) */
+    }) 
 
-   /* if (!movieIsInArray) {
+    if (!movieIsInArray) {
       tempMoviesInFavs.push(movieData);      
       localStorage.setItem('favs', JSON.stringify(tempMoviesInFavs));
-      console.log("Se agrego la peli")            
-    }  else {
-      let moviesLeft = tempMoviesInFavs.filter(oneMovie => {
+      setFavoritos(tempMoviesInFavs);
+      swal("Bien","Se agrego la pelicula a favoritos","success");             
+    } else {
+        let moviesLeft = tempMoviesInFavs.filter(oneMovie => {
         return oneMovie.id !== movieData.id
       })
       localStorage.setItem('favs', JSON.stringify(moviesLeft));
-      console.log("Se elimino la peli") 
-    }  */
+      setFavoritos(moviesLeft);
+      swal("Bien","Se elimino la pelicula de favoritos","success");
+    } 
  
   }
 
   return (
     <BrowserRouter>
-    <Header />
+    <Header favoritos={favoritos} />
       <div className="container">
           <Routes>
             <Route exact path="/" element={<Login />} />     
-            <Route path="/listado"  element={<Listado addOrRemoveFromFavs={addOrRemoveFromFavs} />} />
+            <Route path="/listado"  element={  <Listado addOrRemoveFromFavs={addOrRemoveFromFavs} />} />
             <Route path="/detalle"  element={<Detalle />} />
-            <Route path="/resultados"  element={<Resultados />} />
+            <Route path="/resultados"  element={<Resultados addOrRemoveFromFavs={addOrRemoveFromFavs}  />} />
+            <Route path="/favoritos"  element={<Favoritos favoritos={favoritos} addOrRemoveFromFavs={addOrRemoveFromFavs}  />} />
           </Routes> 
       </div>
-     <Footer /> 
+     
     </BrowserRouter>  
   );
 }
