689e1c94fcb200317dccdcff8c7d3458c9696e95
margaritasing <libiamfreitesm@gmail.com>
2022-07-25 10:50:17 -0300

    ....

--- src/componentes/Listado.js
+++ src/componentes/Listado.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, Navigate } from "react-router-dom";
 import swal from 'sweetalert';
-import styled from "styled-components";
+
 
 
 
 const Listado = (props) => {   
-  console.log(props)
+  
 
   let token  = sessionStorage.getItem('token');
 
@@ -31,7 +31,7 @@ const Listado = (props) => {
   
   return (
     
-    <Section>
+   <>
     {!token && <Navigate replace to="/" />}
     <div className="row my-2 mx-2">
     {
@@ -40,16 +40,16 @@ const Listado = (props) => {
             
                   <div className="col-3" key={index}>
                     <div className="card my-2">
-                      <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="..."/>
+                      <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} style={{height:"300px"}} className="card-img-top" alt="..."/>
                         <button className="favorito"
                         onClick={props.addOrRemoveFromFavs}
                         data-movie-id={oneMovie.id}>🖤
                         </button>
-                      <div className="card-body">
+                       <div className="card-body text-center" style={{height:"220px"}}>
                           <h5 className="card-title">{oneMovie.title.substring(0, 30)}</h5>
-                          <p className="card-text">{oneMovie.overview.substring(0, 100)}...</p>
-                          <Link to={`/detalle?movieID=${oneMovie.id}`} className="btn btn-dark">View details</Link>
-                        </div>
+                          <p className="card-text">{oneMovie.overview.substring(0, 50)}...</p>
+                          <Link to={`/detalle?movieID=${oneMovie.id}`} className="btn btn-dark" >View details</Link>
+                      </div>
                     </div>     
                   </div>           
               
@@ -58,28 +58,12 @@ const Listado = (props) => {
     }
          
     </div>
-    </Section>
     
+    </>
     
   )
 }
 
-const Section = styled.section`   
-.favorito {
-  width: 30px;
-  height: 30px;
-  background-color: #f2f2f2;
-  border-radius: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  right: 10px;
-  top: 10px;
-  border: none;
-  font-size:13px;
-}
- 
-`;
+
 
 export default Listado
